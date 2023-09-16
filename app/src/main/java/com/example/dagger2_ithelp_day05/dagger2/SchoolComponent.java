03b555fbad2e7bb7027b727d220c7536dc7a37ec
Jaychen <jay0345678@gmail.com>
2023-09-16 19:46:14 +0800

    @Component.Builder、@BindsInstance、@Named create

--- app/src/main/java/com/example/dagger2_ithelp_day05/dagger2/SchoolComponent.java
+++ app/src/main/java/com/example/dagger2_ithelp_day05/dagger2/SchoolComponent.java
@@ -7,15 +7,31 @@ import com.example.dagger2_ithelp_day05.module.StudentModule;
 import com.example.dagger2_ithelp_day05.module.TeacherModule;
 import com.example.dagger2_ithelp_day05.school.School;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
+import dagger.BindsInstance;
 import dagger.Component;
 
-//@Singleton
+@Singleton
 @Component(modules = {StudentModule.class,
                     TeacherModule.class,
                     StateSchoolModule.class})
 public interface SchoolComponent {
     School getSchool();
     void inject(MainActivity mainActivity);
+
+    @Component.Builder
+    interface Builder{
+        @BindsInstance
+        Builder score(@Named("score") int score);
+
+        @BindsInstance
+        Builder worldRanking(@Named("worldRanking") int worldRanking);
+
+        @BindsInstance
+        Builder name(String name);
+
+        SchoolComponent build();
+    }
 }
